20ec89e20505cf7a7fc20d5699e3b1bbd561ca85
abdirihaman <42945643+abdirihaman@users.noreply.github.com>
2022-12-16 01:33:05 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
    
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
     <style>
-    @import url("/bootstrap-icons-1.7.1/font/bootstrap-icons.css");
+    @import url("public/bootstrap-icons-1.7.1/font/bootstrap-icons.css");
 /* @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@400&display=swap'); */
 
 @font-face {
